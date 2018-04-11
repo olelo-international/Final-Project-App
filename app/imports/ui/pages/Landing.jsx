d95ee0449e10ceb0faba828e94e57a96f1bac5f4
Sabine Strasburger <sstrasburger451@gmail.com>
2018-04-10 14:32:30 -1000

    landing page mockup finished

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='olelo-landing-background'>
+          <Grid container stackable centered columns={1}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <div className='olelo-landing-format'>
+                <div className='animated-sentence'>
+                  <span><Header as='h1' inverted>Connect with UH Manoa students and improve your foreign language
+                    studies!</Header></span>
+                  <span><Header as='h1' inverted>E hui me nā haumāna Manoa a me ka hoʻomaikaʻiʻana i kāu
+                    mau&apos;ōlelo&apos;oko&apos;a haole!</Header></span>
+                  <span><Header as='h1' inverted>Connectez-vous avec les étudiants de UH Manoa et améliorez vos études
+                    de langues étrangères!</Header></span>
+                  <span><Header as='h1' inverted>¡Conéctese con los estudiantes de UH Manoa y mejore sus estudios de
+                    idiomas!</Header></span>
+                </div>
+              </div>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+          </Grid>
+        </div>
     );
   }
 }
