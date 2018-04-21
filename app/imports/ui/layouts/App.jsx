43c6b8c065efc34cc145e4f45325f30c69ef7495
Sabine Strasburger <sstrasburger451@gmail.com>
2018-04-21 09:39:25 -1000

    Added description to Landing

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -15,6 +15,7 @@ import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+import MiddleFooter from '../components/MiddleFooter';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -34,6 +35,7 @@ class App extends React.Component {
               <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
             </Switch>
+            <MiddleFooter/>
             <Footer/>
           </div>
         </Router>
